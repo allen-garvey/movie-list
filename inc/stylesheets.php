12eb0970fef4a3eaa17b73a0fa870f2a912b1bff
Allen Garvey <allengarvey@hotmail.com>
2015-06-15 01:49:21 -0400

    delete stylesheets file and move into head view file

--- inc/stylesheets.php
+++
@@ -1,3 +0,0 @@
-<link rel='stylesheet' type='text/css' href='styles/bootstrap.min.css'/>
-<link rel='stylesheet' type='text/css' href='styles/bootstrap-theme.min.css'/>
-<link rel='stylesheet' type='text/css' href='styles/style.css'/>
---
+++ inc/views/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $page_controller->get_title() ?></title>
+		<link rel='stylesheet' type='text/css' href='styles/bootstrap.min.css'/>
+		<link rel='stylesheet' type='text/css' href='styles/bootstrap-theme.min.css'/>
+		<link rel='stylesheet' type='text/css' href='styles/style.css'/>
+	</head>
+<body>
