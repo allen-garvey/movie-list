f1990b6b4e1b38863dbcf03926817892cdf0c9a6
Allen Garvey <allengarvey@hotmail.com>
2015-06-15 22:48:05 -0400

    add new constants to config.php for styles and scripts url and inc, views and controllers paths

--- inc/config.php
+++ inc/config.php
@@ -10,4 +10,10 @@ if(ENVIRONMENT_CURRENT === ENVIRONMENT_LOCAL){
 define('HOME_URL', BASE_URL.'index.php');
 define('RATED_URL', BASE_URL.'rated.php');
 define('SUGGESTIONS_URL', BASE_URL.'suggestions.php');
+define('STYLES_URL', BASE_URL.'styles/');
+define('SCRIPTS_URL', BASE_URL.'scripts/');
+
+define('INC_PATH', ROOT_PATH.'inc/');
+define('VIEWS_PATH', INC_PATH.'views/');
+define('CONTROLLERS_PATH', INC_PATH.'controllers/');
 
