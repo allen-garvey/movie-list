76b438138f88adb3123dd2e771f122aa020ef6dc
Allen Garvey <allengarvey@hotmail.com>
2015-06-16 14:59:47 -0400

    add constants for errors and min and max rating values for server-side validation

--- inc/constants.php
+++ inc/constants.php
@@ -9,6 +9,24 @@ class Movie_List_Constants
 	//ratings are inculsive ie good >= $good_movie_rating, ok >= $ok_movie_rating, bad < $ok_movie_rating
 	public static $good_movie_rating = 85;
 	public static $ok_movie_rating = 70;
+
+	//*****for validation of adding and editing movies
+	public static $min_rating = 1;
+	public static $max_rating = 100;
+
+
+	//*******for error messages
+	public static $title_error = 'Movies must have a title.';
+	public static $rating_error;
+	public static $pre_rating_error;
+	public static $post_rating_error;
+
+	public static function init(){
+		//php can't do non-trivial initialization
+		self::$rating_error = 'Please supply a number between ' . self::$min_rating . ' and ' . self::$max_rating;
+		self::$pre_rating_error = self::$rating_error . ' for pre-rating.';
+		self::$post_rating_error = self::$rating_error . ' for post-rating.';
+	}
 }
 
-?>
+Movie_List_Constants::init();
