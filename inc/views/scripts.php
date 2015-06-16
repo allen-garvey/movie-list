737ab870cc38c931d4196a75a0a6b08be442db5a
Allen Garvey <allengarvey@hotmail.com>
2015-06-15 22:50:26 -0400

    move js tags into separate scripts view file with new scripts_url constant

---
+++ inc/views/scripts.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src='<?= SCRIPTS_URL; ?>jquery-2.1.3.min.js'></script>
+<script type="text/javascript" src='<?= SCRIPTS_URL; ?>bootstrap.min.js'></script>
+<script type="text/javascript" src='<?= SCRIPTS_URL; ?>angular.min.js'></script>
+<script type="text/javascript" src='<?= SCRIPTS_URL; ?>app.js'></script>
