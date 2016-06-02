2815758ed810a7baaa0d131ac7dfdf8d19cc1305
Allen Garvey <allengarvey@hotmail.com>
2016-06-01 23:19:47 -0400

    remove scripts for angular

--- inc/views/scripts.php
+++ inc/views/scripts.php
@@ -1,4 +1,3 @@
 <script type="text/javascript" src='<?= SCRIPTS_URL; ?>jquery-2.1.3.min.js'></script>
 <script type="text/javascript" src='<?= SCRIPTS_URL; ?>bootstrap.min.js'></script>
-<script type="text/javascript" src='<?= SCRIPTS_URL; ?>angular.min.js'></script>
-<script type="text/javascript" src='<?= SCRIPTS_URL; ?>app.js'></script>
+<script type="text/javascript" src='<?= SCRIPTS_URL; ?>app2.js'></script>
