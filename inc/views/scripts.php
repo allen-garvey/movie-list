7206fcdc5d062bc1805ce39cec4a28612197a63b
Allen Garvey <allengarvey@hotmail.com>
2016-06-04 13:07:14 -0400

    update jquery to 2.2.4

--- inc/views/scripts.php
+++ inc/views/scripts.php
@@ -1,4 +1,4 @@
-<script src='<?= SCRIPTS_URL; ?>jquery-2.1.3.min.js'></script>
+<script src='<?= SCRIPTS_URL; ?>jquery-2.2.4.min.js'></script>
 <script src='<?= SCRIPTS_URL; ?>bootstrap.min.js'></script>
 <script>
 	var config = {};
