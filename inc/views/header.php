232610cb331cd449057fbc41e3fe40e32f6a0ea1
Allen Garvey <allengarvey@hotmail.com>
2016-09-24 20:45:50 -0400

    Make site title a link to the homepage

--- inc/views/header.php
+++ inc/views/header.php
@@ -1,4 +1,5 @@
-<header class='jumbotron'><h1 class='main_title'>Movie List</h1>
+<header class='jumbotron'>
+	<h1 class='main_title'><a href='<?= BASE_URL; ?>'>Movie List</a></h1>
 	<nav>
 		<ul class='nav nav-pills'>
 			<?= $page_controller->get_nav_items(); ?>
