60072cbb4980b95070e1d217572b2de08ecf590d
Allen Garvey <allengarvey@hotmail.com>
2016-10-01 14:00:02 -0400

    Refactor to use htaccess and php routing for page requests instead of separate files

--- inc/views/layout.php
+++ inc/views/layout.php
@@ -1,12 +1,4 @@
 <?php
-require_once('../inc/config.php');
-require_once(CONTROLLERS_PATH.'page_controller.php');
-
-$page_controller = new AGED_Suggestions_Controller();
-$movie_genre_result = AGED_Page_Controller::get_movie_genre_result();
-?>
-
-<?php 
 	include(VIEWS_PATH.'head.php');
 	include(VIEWS_PATH.'header.php'); 
 ?>
@@ -17,9 +9,12 @@ $movie_genre_result = AGED_Page_Controller::get_movie_genre_result();
 				<?php include(VIEWS_PATH.$page_controller->get_name().'_table_headings.php'); ?>
 			</tr>
 		</thead>
-		<tbody>		
+		<tbody>
 			<?= $page_controller->get_table_content_rows(); ?>
 		</tbody>
 	</table>
+	<div class='text-center add_button'>
+		<button type="button" class='btn btn-lg btn-primary' id="add-movie-button">Add Movie</button>
+	</div>
 </main>
 <?php include(VIEWS_PATH.'footer.php'); ?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,30 +1,19 @@
 <?php 
 require_once('../inc/config.php');
-include_once(CONTROLLERS_PATH.'page_controller.php');
-require_once(CONTROLLERS_PATH.'localhost_database_pg.php');
-$page_controller = new AGED_Index_Controller();
+require_once(CONTROLLERS_PATH.'page_controller.php');
+
+//routing
+$path = array_key_exists('path', $_GET) ? $_GET['path'] : '';
+if(preg_match('/^suggestions/', $path)){
+	$page_controller = new AGED_Suggestions_Controller();
+}
+else if(preg_match('/^rated/', $path)){
+	$page_controller = new AGED_Rated_Controller();
+}
+else{
+	$page_controller = new AGED_Index_Controller();	
+}
+
 $movie_genre_result = AGED_Page_Controller::get_movie_genre_result();
 
-?>
-
-<?php 
-	include(VIEWS_PATH.'head.php');
-	include(VIEWS_PATH.'header.php'); 
-?>
-	<main>
-		<table id='movie_table'>
-			<thead>
-				<tr>
-					<?php include(VIEWS_PATH.$page_controller->get_name().'_table_headings.php'); ?>
-				</tr>
-			</thead>
-			<tbody>
-				<?= $page_controller->get_table_content_rows(); ?>
-			</tbody>
-		</table>
-		<div class='text-center add_button'>
-			<button type="button" class='btn btn-lg btn-primary' id="add-movie-button">Add Movie</button>
-		</div>
-	</main>
-	<?php include(VIEWS_PATH.'footer.php'); ?>
-
+include(VIEWS_PATH.'layout.php');
