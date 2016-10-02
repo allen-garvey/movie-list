3f2b63f9dbd361868a4d4222820c62630a6de671
Allen Garvey <allengarvey@hotmail.com>
2016-10-02 15:22:04 -0400

    Reorder constants for clarity and add app title constant

--- inc/config.php
+++ inc/config.php
@@ -2,11 +2,16 @@
 define('ENVIRONMENT_LOCAL', 0);
 define('ENVIRONMENT_CURRENT', ENVIRONMENT_LOCAL);
 
+//Path constants
+define('ROOT_PATH', dirname(__FILE__, 2).'/');
+define('INC_PATH', ROOT_PATH.'inc/');
+define('VIEWS_PATH', INC_PATH.'views/');
+define('CONTROLLERS_PATH', INC_PATH.'controllers/');
+
+//Url Constants
 define('DOMAIN', getenv('MULTISITE_DOMAIN'));
 define('BASE_URL','http://movies.'.DOMAIN.'/');
 define('SUPER_SEARCH_URL', 'http://search.'.DOMAIN.'/index.php?q=');
-define('ROOT_PATH', dirname(__FILE__, 2).'/');
-
 
 define('API_URL', BASE_URL.'api/');
 define('HOME_URL', BASE_URL.'');
@@ -15,7 +20,6 @@ define('SUGGESTIONS_URL', BASE_URL.'suggestions/');
 define('STYLES_URL', BASE_URL.'styles/');
 define('SCRIPTS_URL', BASE_URL.'scripts/');
 
-define('INC_PATH', ROOT_PATH.'inc/');
-define('VIEWS_PATH', INC_PATH.'views/');
-define('CONTROLLERS_PATH', INC_PATH.'controllers/');
+//Application constants
+define('APP_TITLE', 'Movie List');
 
