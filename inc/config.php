65b3db8e348074b5bbd98a5a5e14f87c44c5e6bf
Allen Garvey <allengarvey@hotmail.com>
2016-10-09 14:59:57 -0400

    Add separate config file for multisite

--- inc/config.php
+++ inc/config.php
@@ -8,9 +8,11 @@ define('INC_PATH', ROOT_PATH.'inc/');
 define('VIEWS_PATH', INC_PATH.'views/');
 define('CONTROLLERS_PATH', INC_PATH.'controllers/');
 
+define('SUBDOMAIN', 'movies');
+
+require_once(INC_PATH.'multisite-config.php');
+
 //Url Constants
-define('DOMAIN', getenv('MULTISITE_DOMAIN'));
-define('BASE_URL','http://movies.'.DOMAIN.'/');
 define('SUPER_SEARCH_URL', 'http://search.'.DOMAIN.'/index.php?q=');
 
 define('API_URL', BASE_URL.'api/');
---
+++ inc/multisite-config.php
@@ -0,0 +1,4 @@
+<?php 
+
+define('DOMAIN', getenv('MULTISITE_DOMAIN'));
+define('BASE_URL','http://'.SUBDOMAIN.'.'.DOMAIN.'/');
